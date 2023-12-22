298556458bf8703daa81e9714065b054d347740d
Rafaela Ridolphi <rafaela-ridolphi@hotmail.com>
2023-12-22 09:15:46 +0100

    projeto final

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function Appp() {
+  return (
+  <div className="App">
+
+  </div>;
+  );
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import "./styles.css";
+import App from "./pages/home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
